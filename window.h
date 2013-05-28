0780f6234820e4ed55bddb60c0d70f06355a72ce
Jamie <jamie@cerretelli.co.uk>
2013-05-28 15:34:59 +0100

    updated

--- window.h
+++ window.h
@@ -20,21 +20,16 @@ typedef struct WIDGETS
 	GtkWidget *window;
 	GtkWidget *entry_password;
 	GtkWidget *cbo_size;
-	GtkWidget *btn_ok;
 	GtkWidget *notebook;
 	GtkWidget *text_public;
 	GtkWidget *text_private;
 	GtkWidget *log_window;
-	//
 	GtkWidget *entry_open;
-	GtkWidget *entry_save;
 	GtkWidget *btn_open;
-	GtkWidget *btn_save;
 	GtkWidget *btn_encrypt;
 	GtkWidget *btn_decrypt;
-	GtkEntryBuffer *buffer_open;
-	GtkEntryBuffer *buffer_save;
 	
+	GtkEntryBuffer *buffer_open;
 	GtkEntryBuffer *buffer_password;
 	GtkTextBuffer *buffer_public;
 	GtkTextBuffer *buffer_private;
@@ -54,10 +49,8 @@ void log_message(_widgets *widgets, gchar *message);
 
 // callbacks
 void on_window_destroy(GtkWidget *widget, gpointer data);
-void btn_ok_clicked(GtkWidget *widget, gpointer data);
 void btn_open_clicked(GtkWidget *widget, gpointer data);
-void btn_save_clicked(GtkWidget *widget, gpointer data);
 void btn_encrypt_clicked(GtkWidget *widget, gpointer data);
-void btn_decrypt_clicked(GtkWidget *widget, gpointer data);
+void clear_log(_widgets *widgets);
 
 #endif
